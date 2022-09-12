5396ab49b8af0c8119443f4a61d18f99da4c000b
salaimuniselvam <salaimuniselvams@geakminds.com>
2022-09-12 18:28:06 +0530

    contract update

--- hardhat/constants/index.js
+++ hardhat/constants/index.js
@@ -1,5 +1,5 @@
 const WHITELIST_CONTRACT_ADDRESS = "0xa7f9e1ad14bb84be79e80f5a46cf427ae5d9afeb";
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-rust-tau.vercel.app/api/";
 
 module.exports = {
   WHITELIST_CONTRACT_ADDRESS,
